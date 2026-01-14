f534b710598c6c3a9c1229fc91997d35597d6feb
Emmilie Estabillo <emsestabillo@gmail.com>
2026-01-14 13:38:35 -0500

    Add new showtimes

--- src/components/custom/BulkScheduling.jsx
+++ src/components/custom/BulkScheduling.jsx
@@ -9,14 +9,20 @@ export default function BulkScheduling({
 }) {
   const schedulingPatterns = [
     { label: "Sundays at 3pm", day: "sunday", time: "3:00" },
+    { label: 'Mondays at 7:30pm', day: 'monday', time: '7:30' },
+    { label: "Tuesdays at 3pm", day: "tuesday", time: "3:00" },
     { label: "Tuesdays at 7pm", day: "tuesday", time: "7:00" },
+    { label: "Tuesdays at 7:30pm", day: "tuesday", time: "7:30" },
     { label: "Wednesdays at 2pm", day: "wednesday", time: "2:00" },
     { label: "Wednesdays at 7:30pm", day: "wednesday", time: "7:30" },
     { label: "Thursdays at 7pm", day: "thursday", time: "7:00" },
+    { label: "Thursdays at 7:30pm", day: "thursday", time: "7:30" },
     { label: "Fridays at 7pm", day: "friday", time: "7:00" },
+    { label: "Fridays at 8pm", day: "friday", time: "8:00" },
     { label: "Saturdays at 2pm", day: "saturday", time: "2:00" },
-    { label: "Saturdays at 7:00pm", day: "saturday", time: "7:00" },
+    { label: "Saturdays at 3:00pm", day: "saturday", time: "3:00" },
     { label: "Saturdays at 7:30pm", day: "saturday", time: "7:30" },
+    { label: "Saturdays at 8:00pm", day: "saturday", time: "8:00" },
   ];
 
   const dayOrder = [
