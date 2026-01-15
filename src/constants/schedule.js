1b2002fb6cf628084f1ff1389b730444be6b15d0
Emmilie Estabillo <emsestabillo@gmail.com>
2026-01-15 17:28:51 -0500

    Add showtimes

--- src/constants/schedule.js
+++ src/constants/schedule.js
@@ -1,13 +1,21 @@
 export const SCHEDULING_PATTERNS = [
   { label: "Sundays at 3pm", day: "sunday", time: "3:00" },
   { label: "Tuesdays at 7pm", day: "tuesday", time: "7:00" },
+  { label: "Tuesdays at 7:30pm", day: "tuesday", time: "7:30" },
+  { label: "Tuesdays at 8:00pm", day: "tuesday", time: "8:00" },
   { label: "Wednesdays at 2pm", day: "wednesday", time: "2:00" },
   { label: "Wednesdays at 7:30pm", day: "wednesday", time: "7:30" },
+  { label: "Wednesdays at 8:00pm", day: "wednesday", time: "8:00" },
   { label: "Thursdays at 7pm", day: "thursday", time: "7:00" },
+  { label: "Thursdays at 7:30pm", day: "thursday", time: "7:30" },
+  { label: "Thursdays at 8:00pm", day: "thursday", time: "8:00" },
   { label: "Fridays at 7pm", day: "friday", time: "7:00" },
+  { label: "Fridays at 7:30pm", day: "friday", time: "7:30" },
+  { label: "Fridays at 8:00pm", day: "friday", time: "8:00" },
   { label: "Saturdays at 2pm", day: "saturday", time: "2:00" },
   { label: "Saturdays at 7:00pm", day: "saturday", time: "7:00" },
   { label: "Saturdays at 7:30pm", day: "saturday", time: "7:30" },
+  { label: "Saturdays at 8:00pm", day: "saturday", time: "8:00" },
 ];
 
 export const DAY_ORDER = [
